92cd8c8462a4f02647e5b9c711f09d0ff4bcffa0
Gustavo <gussanma@alumnos.ubiobio.cl>
2017-04-26 12:06:35 -0400

    segundo comit , primera prueba

--- src/main/java/cl/ubb/Game.java
+++ src/main/java/cl/ubb/Game.java
@@ -1,5 +1,16 @@
 package cl.ubb;
 
 public class Game {
+	
+	int score;
+	public Game(){
+		score=0;
+	}
+	public int Score(){
+		return score;
+	}
+	public void Roll(int pins){
+		score+=pins;
+	}
 
 }
--- src/test/java/cl/ubb/GameTest.java
+++ src/test/java/cl/ubb/GameTest.java
@@ -2,13 +2,28 @@ package cl.ubb;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+
+
 public class GameTest {
+	
+	Game gm;
+	int result;
+
+	@Before
+	public void setUp(){
+		gm = new Game();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void rollthreeAndRollFiveShoulbeEight() {
+		gm.Roll(3);
+		gm.Roll(5);
+		result=gm.Score();
+		
+		assertEquals(8,result);
 	}
 
 }
